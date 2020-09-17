b27e16d9350eea628ed3e58296cd02ba26c834fc
Ritesh Panchal <riteshpanchal16@gmail.com>
2020-09-17 16:49:59 +0530

    initialize speech recognaization program

---
+++ speech.py
@@ -0,0 +1,39 @@
+import speech_recognition as sr
+import webbrowser
+import time
+r = sr.Recognizer()
+def  record_audio(ask= False):
+    with sr.Microphone() as source:
+        if ask:
+            print(ask)
+        audio = r.listen(source)
+        voice_data=''
+        try:
+            voice_data = r.recognize_google(audio)
+            print(voice_data)
+        except sr.UnknownValueError:
+            print('Sorry, I did not get that')
+        except sr.RequestError:
+            print('Sorry, my speech services is down')
+        return voice_data
+
+def respond(voice_data):
+    if 'what is your name' in voice_data:
+        print('my name is Jarvis')
+    if 'what time is it' in voice_data:
+        print(time.ctime())
+    if 'what' in voice_data:
+        search=record_audio('What do you want to search for?')
+        url='http://google.com/search?q='+ search
+        webbrowser.get().open(url)
+        print('Here is what I found for'+ search)
+    if 'location' in voice_data:
+        search= record_audio('which location')
+        url='https://www.google.co.in/maps/place/'+search
+        webbrowser.get().open(url)
+        print("location" + search)
+
+        
+print('How can i help you?')
+voice_data=record_audio()
+respond(voice_data)
